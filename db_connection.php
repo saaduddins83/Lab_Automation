16591103e4d38e56fed17d0c2271452ee151315e
Saad Uddin <saaduddins83@gmail.com>
2024-05-12 19:11:34 +0500

    update db_connection

--- db_connection.php
+++ db_connection.php
@@ -1,3 +1,5 @@
 <?php
 $con = mysqli_connect("localhost","root","","lab_automation");
+// $con = mysqli_connect("sql112.infinityfree.com:3306","if0_36533462","XPX8b3iS0UHi", "if0_36533462_lab_automation");
+
 ?>
